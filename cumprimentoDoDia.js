74a72a55a75a4e4f04a1b3436f58b90d11dafd10
JanianaCiteli <12562399+JanianaCiteli@users.noreply.github.com>
2021-11-24 00:01:35 -0300

    Add files via upload

---
+++ cumprimentoDoDia.js
@@ -0,0 +1,17 @@
+let bomDia = function(){
+	return "bom dia"
+}
+let boaTarde = function(){
+	return "boa tarde"
+}
+let data = new Date();
+let cumprimento;
+
+if(data.getSeconds()<30){
+	cumprimento = bomDia;
+}else{
+	cumprimento = boaTarde;
+};
+f(){
+	return "boa tarde"
+}
